770ea7de630c7625301515093e085d5a928c1582
Jim Posen <jim.posen@gmail.com>
2012-09-20 22:18:23 -0400

    Construct has name of type. First egregious hack.

--- test/unit/monster.js
+++ test/unit/monster.js
@@ -8,5 +8,9 @@ describe('monster', function () {
         it('should return a new constructor function', function () {
             monster.define('Monster').should.be.a('function');
         });
+
+        it('should return a function with name matching argument', function () {
+            monster.define('Monster').name.should.equal('Monster');
+        });
     });
 });
