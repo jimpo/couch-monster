0484c64d97eae3461e8f928abdcedcd9672c0801
Jim Posen <jim.posen@gmail.com>
2012-09-20 23:16:35 -0400

    Constructor sets id and attributes appropriately.

--- test/unit/monster.js
+++ test/unit/monster.js
@@ -5,26 +5,49 @@ var monster = require('monster');
 
 describe('monster', function () {
     describe('#define()', function () {
+        var Monster;
+
+        beforeEach(function () {
+            Monster = monster.define('Monster');
+        });
+
         it('should return a new constructor function', function () {
-            monster.define('Monster').should.be.a('function');
+            Monster.should.be.a('function');
         });
 
         it('should return a function with name matching argument', function () {
-            monster.define('Monster').name.should.equal('Monster');
+            Monster.name.should.equal('Monster');
         });
-    });
 
-    describe('defined Monster', function () {
-        var Monster, options;
+        it('should set id to given id', function () {
+            var marvin = new Monster('marvin');
+            marvin.attributes._id.should.equal('marvin');
+        });
 
-        before(function () {
-            options = {
+        it('should initialize attributes to constructor argument', function () {
+            var marvin = new Monster('marvin', {
+                location: 'closet',
+                scary: true,
+            });
+            marvin.attributes.should.deep.equal({
+                _id: 'marvin',
+                location: 'closet',
+                scary: true,
+            });
+        });
+
+        it('should initialize attributes to empty object otherwise',
+           function () {
+               var marvin = new Monster();
+               expect(marvin.attributes).to.exist;
+               marvin.attributes.should.deep.equal({});
+           });
+
+        it('constructor should call initialize on object', function () {
+            var options = {
                 initialize: sinon.spy(),
             };
-            Monster = monster.define('Monster', options);
-        });
-
-        it('should call initialize in constructor', function () {
+            var Monster = monster.define('Monster', options);
             var marvin = new Monster('marvin');
             options.initialize.should.have.been.calledOn(marvin);
         });
