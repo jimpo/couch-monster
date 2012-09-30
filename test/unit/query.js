b58b041469582646218e033c50e6c052c8f78f39
Jim Posen <jim.posen@gmail.com>
2012-09-29 23:33:19 -0400

    Wrote first modifiers.

--- test/unit/query.js
+++ test/unit/query.js
@@ -5,20 +5,6 @@ var Query = require('query');
 
 
 describe('Query', function () {
-    var mock;
-
-    beforeEach(function () {
-        monster.db = {
-            view: function(){},
-        };
-        mock = sinon.mock(monster.db);
-    });
-
-    afterEach(function () {
-        mock.verify();
-        mock.restore();
-    });
-
     describe('constructor', function () {
         it('should set options to constructor argument', function () {
             var options = {descending: true};
@@ -44,7 +30,57 @@ describe('Query', function () {
         });
     });
 
-    describe('execute', function () {
+    describe('modifiers', function () {
+        var query;
+
+        beforeEach(function () {
+            query = new Query;
+        });
+
+        describe('#key()', function () {
+            it('should set key option to argument', function () {
+                query.key('marvin').should.equal(query);
+                query.options.key.should.equal('marvin');
+            });
+        });
+
+        describe('#keys()', function () {
+            it('should set keys option to arguments list', function () {
+                query.keys('marvin', 'charlie').should.equal(query);
+                query.options.keys.should.deep.equal(['marvin', 'charlie']);
+            });
+
+            it('should set keys option to array if one argument given',
+               function () {
+                   query.keys(['marvin', 'charlie']).should.equal(query);
+                   query.options.keys.should.deep.equal(['marvin', 'charlie']);
+               });
+        });
+
+        describe('#start()', function () {
+
+        });
+
+        describe('#end()', function () {
+
+        });
+    });
+
+    describe('#execute()', function () {
+        var mock;
+
+        beforeEach(function () {
+            monster.db = {
+                view: function(){},
+            };
+            mock = sinon.mock(monster.db);
+        });
+
+        afterEach(function () {
+            mock.verify();
+            mock.restore();
+        });
+
         it('should call db.view with design name, view, and options',
            function () {
                var query = new Query('design', 'view');
