dec600eca11149f006efde546f2b9da0a2f6b11b
Jim Posen <jim.posen@gmail.com>
2012-09-30 12:14:12 -0400

    Wrote all simple modifiers.

--- test/unit/query.js
+++ test/unit/query.js
@@ -58,11 +58,98 @@ describe('Query', function () {
         });
 
         describe('#start()', function () {
+            it('should set startkey option to first argument', function () {
+                query.start('marvin').should.equal(query);
+                query.options.startkey.should.equal('marvin');
+            });
 
+            it('should set startkey_docid if second argument given',
+               function () {
+                   query.start('marvin', '12345').should.equal(query);
+                   query.options.startkey_docid.should.equal('12345');
+               });
         });
 
         describe('#end()', function () {
+            it('should set endkey option to first argument', function () {
+                query.end('marvin').should.equal(query);
+                query.options.endkey.should.equal('marvin');
+            });
 
+            it('should set endkey_docid if second argument given',
+               function () {
+                   query.end('marvin', '12345').should.equal(query);
+                   query.options.endkey_docid.should.equal('12345');
+               });
+        });
+
+        describe('#limit', function () {
+            it('should set limit option to argument', function () {
+                query.limit(5).should.equal(query);
+                query.options.limit.should.equal(5);
+            });
+        });
+
+        describe('#stale()', function () {
+            it('should set stale option to ok', function () {
+                query.stale().should.equal(query);
+                query.options.stale.should.equal('ok');
+            });
+        });
+
+        describe('#staleUpdateAfter()', function () {
+            it('should set stale option to update_after', function () {
+                query.staleUpdateAfter().should.equal(query);
+                query.options.stale.should.equal('update_after');
+            });
+        });
+
+        describe('#descending()', function () {
+            it('should set descending option to true', function () {
+                query.descending().should.equal(query);
+                query.options.descending.should.be.true;
+            });
+        });
+
+        describe('#skip()', function () {
+            it('should set skip option to argument', function () {
+                query.skip(5).should.equal(query);
+                query.options.skip.should.equal(5);
+            });
+        });
+
+        describe('#group()', function () {
+            it('should set group option to true', function () {
+                query.group().should.equal(query);
+                query.options.group.should.be.true;
+            });
+
+            it('should set group_level option if integer given', function () {
+                query.group(5).should.equal(query);
+                query.options.should.not.have.property('group');
+                query.options.group_level.should.equal(5);
+            });
+        });
+
+        describe('#reduce()', function () {
+            it('should set reduce option to true', function () {
+                query.reduce().should.equal(query);
+                query.options.reduce.should.be.true;
+            });
+        });
+
+        describe('#includeDocs()', function () {
+            it('should set include_docs option to true', function () {
+                query.includeDocs().should.equal(query);
+                query.options.include_docs.should.be.true;
+            });
+        });
+
+        describe('#inclusiveEnd()', function () {
+            it('should set inclusive_end option to true', function () {
+                query.inclusiveEnd().should.equal(query);
+                query.options.inclusive_end.should.be.true;
+            });
         });
     });
 
